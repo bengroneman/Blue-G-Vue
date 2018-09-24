8b0bbed420787308ae0a0183c961a522edda2880
Ben Groneman <bluegroneman10@gmail.com>
2018-09-23 22:09:02 -0600

    big changes to the project section

---
+++ src/components/0-Global/bCard.vue
@@ -0,0 +1,88 @@
+<template>
+<div class="card b-grid-child__item">
+  <img class="card-img-top" :src="imgsrc" :alt="imgsrc">
+  <div class="card-body">
+  <div class="card-skills">
+    <span v-if="skillisJS" class="b-skill b-skill-is-js">Javascript</span>
+    <span v-if="skillisVUE" class="b-skill b-skill-is-vue">VueJS</span>
+    <span v-if="skillisWP" class="b-skill b-skill-is-wp">Wordpress</span>
+		<span v-if="skillisHS" class="b-skill b-skill-is-hs">Hubspot</span>
+		<span v-if="skillisPY" class="b-skill b-skill-is-py">Python</span>
+		<span v-if="skillisPHP" class="b-skill b-skill-is-php">PHP</span>
+  </div>
+    <h5 class="text-is-left card-title">{{ cardtitle }}</h5>
+    <h6 class="text-is-left card-sub-title">{{ cardsubtitle }}</h6>
+    <p class="text-is-left card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'bcard',
+  props: {
+		imgsrc: String,
+		cardtitle: String,
+		cardsubtitle: String,
+		cardcontent: String,
+		skillisJS: Boolean,
+		skillisVUE: Boolean,
+		skillisWP: Boolean,
+		skillisHS: Boolean,
+		skillisPY: Boolean,
+		skillisPHP: Boolean
+	}
+}
+</script>
+<style>
+.b-grid-child__item {
+	box-shadow:	0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);	
+}
+.b-grid-child__item:hover {
+	transition: box-shadow .25s;
+	box-shadow: 0 8px 17px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+}
+.b-grid-child__item > img {
+	object-fit: cover;
+	position: relative;
+	background-color: #fff;
+	transition: box-shadow .25s;
+	border-radius: 2px;
+}
+.card-skills {
+	font-size: .9rem;
+	display: inline-flex;
+	border-radius: 3px;
+	margin-right: 5px;
+	margin-bottom: 15px;
+}
+.b-skill {
+	margin-right: 3px;
+	margin-bottom: 15px;
+	background: transparent;
+}
+.card-skills span {
+	border-radius: 5px;
+	padding: 3px 5px;
+}
+.b-skill-is-vue {
+	background: #4fc08d;
+}
+.b-skill-is-js {
+	background: #EADD86;
+}
+.b-skill-is-wp {
+	background: #00aadc;
+}
+.b-skill-is-hs {
+	background: #ff7a59;
+}
+.b-skill-is-py {
+	background: #2b5b84;
+}
+.b-skill-is-php {
+	background: #8892BF; 
+}
+
+</style>
---
+++ src/views/pages/Projects.vue
@@ -0,0 +1,79 @@
+<template>
+  <section class="container-fluid b-project__section">
+    <div class="row b-grid__row">
+      <div class="col-6 b-grid child">
+
+        <!-- grid item 1 -->
+				<bcard
+					imgsrc="https://bulma.io/images/placeholders/640x320.png"
+					cardtitle="Blue G Portfolio Project"
+					cardsubtitle="A portfolio for Ben Groneman"
+					cardcontent="This project was built by Ben Groneman, with VueJS"
+					skillisVUE=true
+					skillisJS=true
+				></bcard>
+				<!-- grid item 2 -->
+				<bcard
+					imgsrc="https://bulma.io/images/placeholders/640x320.png"
+					cardtitle="Ruggedwerx Project"
+					cardsubtitle="Welding/Fabrication company"
+					cardcontent="I built out the front end of this project under the regime of Wyoming inc"
+					skillisWP=true
+					skillisPHP=true
+					skillisJS=true
+				></bcard>
+				<!-- grid item 3 -->
+				<bcard
+					imgsrc="https://bulma.io/images/placeholders/640x320.png"
+					cardtitle="Bonneville Builders Project"
+					cardsubtitle="Contactors of many sorts"
+					cardcontent="This project was built for you!"
+					skillisWP=true
+					skillisPHP=true
+					skillisJS=true
+
+				></bcard>
+				<!-- grid item 4 -->
+				<bcard
+					imgsrc="https://bulma.io/images/placeholders/640x320.png"
+					cardtitle="Museum of the American West Project"
+					cardsubtitle="Museum based in Lander Wy"
+					cardcontent="This project was built soley by yours truley"
+					skillisWP=true
+					skillisPHP=true
+					skillisJS=true
+				></bcard>
+      </div>
+      <div class="col-6 b-project__info child">
+        <h1>h3llo </h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import bcard from '@/components/0-Global/bCard'
+
+export default {
+  name: 'projects',
+	components: {
+		bcard
+	}
+}
+</script>
+
+<style>
+.b-project__section {
+	height: 100%;
+	margin: 50px;
+	padding: 50px;
+}
+.b-grid__row {
+  height: 100%;
+}
+.b-grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
+
+</style>

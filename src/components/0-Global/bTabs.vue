494bbe0560349f516e82629b2c13eea612289036
Ben Groneman <bluegroneman10@gmail.com>
2018-09-23 22:43:24 -0600

    tabs for projects // ideally they become reusable

---
+++ src/components/0-Global/bTabs.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="tabs__wrapper">
+
+    <div class="tabs">
+      <ul>
+        <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+          <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="tabs-details">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tabs',
+
+  data() {
+    return { tabs: [] };
+  },
+
+  created() {
+    this.tabs = this.$children;
+  },
+
+  methods: {
+    selectTab(selectedTab) {
+      this.tabs.forEach(tab => {
+        tab.isActive = (tab.href == selectedTab.href);
+      });
+    }
+  }
+}
+</script>

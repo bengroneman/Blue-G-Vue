cf4172a40097c086b05f0f6d6bbbdd235b031147
Ben Groneman <bluegroneman10@gmail.com>
2018-10-16 19:30:36 -0600

    feat: added logo component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
     <btoolbar></btoolbar>
     
     <router-view></router-view> 
+    <div class="test">
+      <blogo></blogo>
+    </div>
 
     <!-- Blue G Footer-->
     <bfooter></bfooter>
@@ -13,11 +16,13 @@
 <script>
 import btoolbar from '@/components/0-Global/bToolbar'
 import bfooter from '@/components/Footer/bFooter'
+import blogo from '@/components/0-Global/bLogo'
 
 export default {
     components: {
         btoolbar,
-        bfooter
+        bfooter,
+        blogo
     }
 }
 </script>
---
+++ src/components/0-Global/bLogo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="b-svg__wrapper">
+    <!-- Instantiate the logo -->
+    <img src="img/svg/" alt="ben g logo" />
+  </div>
+</template>
+<script>
+export default {
+  name: 'blogo'
+}
+</script>
+
+<style>
+.b-svg__wrapper {
+  display: flex;
+  margin: auto;
+  height: auto;
+  width: auto;
+}
+</style>

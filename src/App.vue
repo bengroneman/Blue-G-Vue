28219cbd478f7ef9ad92d56573375c5c45ae330e
Ben Groneman <bluegroneman10@gmail.com>
2018-11-18 12:29:55 -0700

    updated root app

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
     <!-- END B Global Toolbar -->
    
     <!-- START B dynamic window content -->
-    <router-view></router-view> 
+    <transition name="page-slide" mode="out-in">
+      <router-view></router-view> 
+    </transition>
     <!-- END B dynamic window content -->
 
     <!-- START B footer-->
@@ -18,10 +20,42 @@ import btoolbar from '@/components/0-Global/bToolbar'
 import bfooter from '@/components/Footer/bFooter'
 
 export default {
-    components: {
-        btoolbar,
-        bfooter
-    }
+  components: {
+    btoolbar,
+    bfooter
+  }
 }
 </script>
+<style>
+.page-slide-enter-active {
+  animation: bounceIn 0.3s;
+}
+.page-slide-leave-active {
+  animation: bounceIn 0.3s reverse;
+}
+
+@keyframes slideIn {
+  from {
+    transform: scaleX(0);
+    transform: scaleY(0);
+  }
+  to {
+    transform: scaleY(100%);
+    transform: scaleX(100%);
+  }
+}
+
+@keyframes bounceIn {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+</style>
 

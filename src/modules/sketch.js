705abfd1ef5b1c74e1446130493fc2cb9b9383fd
Ben Groneman <bluegroneman10@gmail.com>
2018-12-25 11:14:29 -0800

    small lesson steps

--- src/modules/sketch.js
+++ src/modules/sketch.js
@@ -9,59 +9,97 @@ var CI = {}; // canvas interactions
 
 
 let lines = [];
+let _ = sketch;
+
+
 
 // ************************** setup **************************
-sketch.setup = function() {
+_.setup = function() {
     // declaring our canvas size
-    sketch.createCanvas(600, 300);
-    sketch.background(0); //color
-
-    let line = new KochLine();
-    lines.push(line);
-    
-    let canvas_width = sketch.width;
-
-    // Declare our vertices
-    let a = new sketch.createVector(0, 173);
-    let b = new sketch.createVector(canvas_width, 173);
-    let c = new sketch.createVector(canvas_width/2, 173+width*sketch.cos(sketch.radians(30)));
-    console.log(a, b, c);
-
-    let start = sketch.createVector(0, 200);
-    let end = sketch.createVector(sketch.width, 200);
-
-    console.log(lines);
-    lines.push(new KochLine(start, end));
-    console.log(lines);
+    _.createCanvas(360, 360);
+    _.pixelDensity(1);
+    _.background(0); //color
+    _.loadPixels();
 
 };
 
 // ******************************* draw loop *****************
-sketch.draw = function() {
-    sketch.background(0);
-    CI.draw();
+_.draw = function() {
+    // global vars
+    let xr = _.canvas.width;
+    let yr = _.canvas.height;
+
+    let xmin = -2.0;
+    let xmax = 1.0;
+    let ymin = -1.5;
+    let ymax = 1.5;
+
+    //lets get colorfrul
+    let mr0 = 0;
+    let mg0 = 0;
+    let mb0 = 0;
+    _.background(0);
 
     // Lets draw some cool things below here
-    for (line in lines) {
-        console.log(line);
-        line.display();
+    while (mr0 == mg0 || mr0 == mb0 || mg0 == mb0) {
+
+        mr0 = Math.pow(2, Math.ceil(Math.random() * 3 + 3));
+        mg0 = Math.pow(2, Math.ceil(Math.random() * 3 + 3));
+        mb0 = Math.pow(2, Math.ceil(Math.random() * 3 + 3));
+
     }
+
+    let mr1 = 256 / mr0;
+    let mg1 = 256 / mg0;
+    let mb1 = 256 / mb0;
+
+    let maxIt = 256;
+    let x = 0.0;
+    let y = 0.0;
+    let zx = 0.0;
+    let zx0 = 0.0;
+    let zy = 0.0;
+    let zx2 = 0.0;
+    let zy2 = 0.0;
+
+    for (let ky = 0; ky < yr; ky++) {
+        y = ymin + (ymax - ymin) * ky / yr;
+        for ( let kx = 0; kx < xr; kx++) {
+            x = xmin + (xmax - xmin) * kx /xr;
+            zx = x;
+            zy = y;
+            for (let i = 0; i < maxIt; i++) {
+                zx2 = zx *zx;
+                zy2 = zy * zy;
+                if (zx2 + zy2 > 4.0) break;
+                zx0 = zx2 - zy2 + x;
+                zy = 2.0 * zx * zy + y;
+                zx = zx0;
+            }
+            let p = (xr * ky + kx) * 4;
+            pix[p] = i % mr0 * mr1;     // red
+            pix[p + 1] = i % mg0 * mg1; // green
+            pix[p + 2] = i % mb0 * mb1; //blue
+            pix[p + 3] = 255;           // alpha
+        }
+    }
+
 };
 
 
 // ******************* canvas mouse events ********************************
 // mouse event
-sketch.mousePressed = function(mouseButton) {
+_.mousePressed = function(mouseButton) {
     //CI.canvasClick(sketch.mouseX, sketch.mouseY);
 }
 
-sketch.mouseDragged = function() {
+_.mouseDragged = function() {
     //CI.canvasDrag(sketch.mouseX, sketch.mouseY);
     // prevent default
     //return false;
 }
 
-sketch.mouseReleased = function() {
+_.mouseReleased = function() {
     //CI.canvasReleased(sketch.mouseX, sketch.mouseY);
 }
 
@@ -79,9 +117,9 @@ const factorial = (n) => {
 
 // more fun with recursion, and circles
 const drawCircle = (x, y, radius) => {
-    sketch.stroke(0);
-    sketch.noFill();
-    sketch.ellipse(x, y, radius, radius);
+    _.stroke(0);
+    _.noFill();
+    _.ellipse(x, y, radius, radius);
     if (radius > 8) {
         drawCircle(x + radius/2, y, radius/2);
         drawCircle(x - radius/2, y, radius/2);
@@ -95,10 +133,10 @@ const cantor = (x, y, len) => {
     // Our cantor color
     if (len >= 1) {
         
-        sketch.stroke(255);
-        sketch.noFill();
+        _.stroke(255);
+        _.noFill();
 
-        sketch.line(x, y, x + len, y);
+        _.line(x, y, x + len, y);
         y += 20;
         cantor(x, y, len/3);
         cantor(x + len * 2 / 3, y, len / 3);
@@ -106,12 +144,8 @@ const cantor = (x, y, len) => {
 }
 
 // Lets get Classy
-const KochLine = {
-    constructor(start, end) {
-        this.start = start;
-        this.end = end;
-    }
+// Refactored from the Nature Of Code CHAPTER 8: Fractals
+// Author: Daniel Shiffman
 
-}
 
 }, 'canvas_container');

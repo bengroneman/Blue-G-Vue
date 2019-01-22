ad8abaa098f57453887b4bf42d2ae701627d4042
Ben Groneman <bluegroneman10@gmail.com>
2019-01-21 18:45:44 -0700

    init tab structure

---
+++ src/components/0-Global/bTab.vue
@@ -0,0 +1,19 @@
+<template>
+<li class="is-active">
+  <a>
+    <span>
+      <slot></slot>
+    </span>
+  </a>
+</li>
+</template>
+
+<script>
+export default {
+  name: 'btab'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/0-Global/bTabs.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="tabs is-centered is-boxed is-medium">
+    <ul class="tabs--ul">
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'btabs'
+}
+</script>
+
+<style scoped>
+
+
+</style>

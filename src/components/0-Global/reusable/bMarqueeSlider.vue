49cda40c268b1c9cb40ee2af5f967cd9da1c9070
Ben Groneman <bluegroneman10@gmail.com>
2019-04-17 20:46:27 -0600

    feat: marquee slider initialized (WIP)

---
+++ src/components/0-Global/reusable/bMarqueeSlider.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="marquee-slider--wrapper">
+    <div class="is-slider--content">
+      <h2 class="text-xs-center">
+        <em>
+          <slot name="quote"></slot>
+        </em>
+      </h2>
+      <p class="text-cs-center">
+        <slot name="attributed_to"></slot>
+      </p>
+    </div>
+  </section>
+</template>
+<script>
+  export default {
+    name: 'bmarqueeslider',
+  }
+</script>
+<style>
+.marquee-slider--wrapper {
+  height: 100%;
+  width: 100%;
+}
+.is-slider--content {
+  margin: 5% auto;
+  width: 75%;
+}
+@media screen and (max-width: 768px) and (min-width: 300px) {
+  .is-slider--content {
+    margin: 5% 10px auto 10px;
+    width: 100%;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,14 @@
     <bintrosection></bintrosection>
     <!-- Small section // Quote blurb -->
     <div class="quote-blurb fluid__container">
-        <h2 class="text-xs-center"><em>"First, solve the problem. Then, write the code."
-        </em></h2><p class="text-xs-center">-- John Johnson</p>
+      <bmarqueeslider>
+        <template slot="quote">
+          "We build our computer (systems) the way we build our cities: over time, without a plan, on top of ruins." 
+        </template>
+        <template slot="attributed_to">
+          - Ellen Ullman
+        </template>
+      </bmarqueeslider>
     </div>
     <!-- Blue G Skill Section -->
     <bskillsection></bskillsection>
@@ -23,6 +29,7 @@ import bprojectsection from '@/components/4-Section/bProjectSection.vue'
 import bhero from '@/components/1-Hero/bHero'
 import bintrosection from '@/components/2-Section/bIntroSection'
 import bskillsection from '@/components/3-Section/bSkillSection'
+import bmarqueeslider from '@/components/0-Global/reusable/bMarqueeSlider'
 
 export default {
   name: 'home',
@@ -30,7 +37,8 @@ export default {
     bprojectsection,
     bhero,
     bintrosection,
-    bskillsection
+    bskillsection,
+    bmarqueeslider
   }
 }
 </script>
